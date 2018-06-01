c50db44694d9edbc021393bbf75d7295ced7d3c7
Yong Jun <yjthong.2009@smu.edu.sg>
2018-06-01 18:22:03 +0800

    Update data for 2018 P1 registration

--- src/preprocess/scrapper/constants.js
+++ src/preprocess/scrapper/constants.js
@@ -1,3 +1,3 @@
 import _range from 'lodash/range'
 
-export const defaultYearRange = _range(2005, 2018).map(v => v.toString())
+export const defaultYearRange = _range(2005, 2019).map(v => v.toString())
