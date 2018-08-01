20b53f720e7956b1698c8c9b1e4dd155051be341
Yong Jun <yjthong.2009@smu.edu.sg>
2018-08-01 15:18:34 +0800

    Update 31 July

--- src/components/DetailView/DetailCard.vue
+++ src/components/DetailView/DetailCard.vue
@@ -551,7 +551,7 @@ export default {
         return {
           label: 'P1 Registration Exercise',
           year: '2018',
-          lastUpdated: '26 July 2018',
+          lastUpdated: '31 July 2018',
           rows
         }
       }
